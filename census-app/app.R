d8bb853d475d0c9e3e95a2c5b4dbba1ca1320e02
chenyang45 <376727397@qq.com>
2019-06-08 17:12:25 +0800

    concise version 01

--- census-app/app.R
+++ census-app/app.R
@@ -30,27 +30,21 @@ ui <- fluidPage(
 # Server logic ----
 server <- function(input, output) {
   output$map <- renderPlot({
-    data <- switch(input$var, 
-                   "Percent White" = counties$white,
-                   "Percent Black" = counties$black,
-                   "Percent Hispanic" = counties$hispanic,
-                   "Percent Asian" = counties$asian)
+    args <- switch(input$var,
+                   "Percent White" = list(counties$white, "darkgreen", "% White"),
+                   "Percent Black" = list(counties$black, "black", "% Black"),
+                   "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
+                   "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
     
-    color <- switch(input$var, 
-                    "Percent White" = "darkgreen",
-                    "Percent Black" = "black",
-                    "Percent Hispanic" = "darkorange",
-                    "Percent Asian" = "darkviolet")
+    args$min <- input$range[1]
+    args$max <- input$range[2]
     
-    legend <- switch(input$var, 
-                     "Percent White" = "% White",
-                     "Percent Black" = "% Black",
-                     "Percent Hispanic" = "% Hispanic",
-                     "Percent Asian" = "% Asian")
-    
-    percent_map(data, color, legend, input$range[1], input$range[2])
+    do.call(percent_map, args)
   })
 }
+
+
+
 
 # Run app ----
 shinyApp(ui, server)
